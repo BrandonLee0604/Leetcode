3e697fadf39ac75bbde641e534161d4bc64a62b7
malasitan <malasitan1@gmail.com>
2021-09-30 22:29:09 +1000

    30/09/2021

--- CPP/223. Rectangle Area/main.cpp
+++ CPP/223. Rectangle Area/main.cpp
@@ -9,11 +9,16 @@
 class Solution {
 public:
     int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        int area1 = (ax2 - ax1) * (ay2 - ay1), area2 = (bx2 - bx1) * (by2 - by1);
+        int overlapWidth = std::min(ax2, bx2) - std::max(ax1, bx1), overlapHeight = std::min(ay2, by2) - std::max(ay1, by1);
+        int overlapArea = std::max(overlapWidth, 0) * std::max(overlapHeight, 0);
+        return area1 + area2 - overlapArea;
 
     }
 };
 
 int main() {
+    auto res = Solution().computeArea(-3, 0, 3, 4, 0, -1, 9, 2);
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
